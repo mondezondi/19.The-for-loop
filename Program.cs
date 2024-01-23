b7f0298b3b8652df8a30b30fd28348854ab34803
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-23 20:14:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.The_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The while loop for reference");
+            int i = 0;
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine("The for loop");
+            for (i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+        }
+    }
+}
